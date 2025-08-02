1bf6963bb452097f38cda98176079fcb3c82bdc8
Arthur Vargas <arthurvargas@Mac.attlocal.net>
2025-08-02 12:58:46 -0500

    working with some dunder methods and a function for ranking cards

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,5 +1,10 @@
 import collections
 
+
+"""
+Although FrenchDeck implicitly inherits from the object class, most of its functionality
+is not inherited, but comes from leveraging the data model and composition.
+"""
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
@@ -14,4 +19,27 @@ class FrenchDeck:
         return len(self._cards)
 
     def __getitem__(self, position):
+        """
+        Just by implementing __getitem__(), our deck is iterable
+        If collection has no __contains__(), the [] operator does
+        a sequential scan
+        """
         return self._cards[position]
+
+
+suit_values = dict(spades=3,hearts=2,diamonds=1,clubs=0)
+def spades_high(card):
+        """
+        a common system of ranking is by rank (with aces being highest),
+        then by suit in the order of spaces -> hearts -> diamonds -> clubs
+        """
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+  
+
+if __name__ == "__main__":
+     french_deck = FrenchDeck()
+     for card in sorted(french_deck, key=spades_high):
+          print(card)
+    
+    
